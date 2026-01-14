f176723daeaaec75f285a4cba374fb12de35e410
HGK27 <gurhankrmli@gmail.com>
2026-01-14 08:30:58 +0300

    googleIconname change

--- src/components/molecules/GoogleLogin/GoogleLogin.jsx
+++ src/components/molecules/GoogleLogin/GoogleLogin.jsx
@@ -1,6 +1,6 @@
 import styles from "./GoogleLogin.module.scss";
 import Button from "../../atoms/Button/Button";
-import GoogleIcon from "../../icons/googleicon.jsx";
+import GoogleIcon from "../../icons/GoogleIcon.jsx";
 
 export default function GoogleLogin() {
   return (
